f85805b49e17f94cb69ecd58699a15cdbd2f2f22
Полина Черкасова <127506525+cherkaasova@users.noreply.github.com>
2024-12-05 10:09:11 +0300

    Update task2.c

--- 3824Б1ПР1/29 Черкасова Полина Денисовна/task2.c
+++ 3824Б1ПР1/29 Черкасова Полина Денисовна/task2.c
@@ -1,6 +1,11 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include<stdio.h>
+#include<limits.h>
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+	unsigned int p = a * b;
+	if (p >= INT_MAX) return UNIT_MAX;
+	if (p <= INT_MIN) return UNIT_MIN;
+	return p;
 }
